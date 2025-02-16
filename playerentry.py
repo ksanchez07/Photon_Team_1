c580b33f4ca1737c729314d6f6ac52ec15927901
nlw008 <nlw008@uark.edu>
2025-02-15 18:29:29 -0600

    Update playerentry.py
    convert playerentry screen to a class

--- playerentry.py
+++ playerentry.py
@@ -1,45 +1,50 @@
 import tkinter
 from tkinter import *
 
-m = tkinter.Tk(screenName="player entry", baseName="photon", className="photon")
-m.geometry("800x600")
-m.configure(bg='black')
 
-playerID = None
-playerCodename = None
+class PlayerEntry:
+    def __init__(self):
+        #self.playerID = None
+        #self.playerCodename = None
+        self.m = tkinter.Tk(screenName="player entry", baseName="photon", className="photon")
+        self.m.geometry("1400x800+100+100")
+        self.m.configure(bg='black')
+        self.create_widgets()
 
-def validate_id(input):
-    if input.isdigit() or input == "":
-        return True
-    else:
-        return False
+    def validate_id(self, input):
+      if input.isdigit() or input == "":
+            return True
+      else:
+            return False
 
-def pushPlayers():
-    global playerID, playerCodename
-    playerID = playerIdEntry.get()
-    playerCodename = playerCodenameEntry.get()
-    if not playerID or not playerCodename:
+    def pushPlayers(self):
+      global playerID, playerCodename
+      #playerID = playerIdEntry.get()
+      #playerCodename = playerCodenameEntry.get()
+      if not playerID or not playerCodename:
         print("Both entries must be filled")
         return
-    print(f"Player ID: {playerID}, Player Codename: {playerCodename}")
+      #print(f"Player ID: {playerID}, Player Codename: {playerCodename}")
+
+      self.playerIdEntry.delete(0, END)
+      self.playerCodenameEntry.delete(0, END) 
+
+    def create_widgets(self):  
+      validate_id_cmd = self.m.register(self.validate_id)
+
+      idLabel = Label(self.m, text="ID")
+      codename = Label(self.m, text="Codename")
+      idLabel.grid(row=0, column=0)
+      codename.grid(row=0, column=1)
+
+      self.playerIdEntry = Entry(self.m, validate="key", validatecommand=(validate_id_cmd, '%P'))
+      self.playerCodenameEntry = Entry(self.m)
+
+      self.playerIdEntry.grid(row=1, column=0)
+      self.playerCodenameEntry.grid(row=1, column=1)
+
+      submitButton = Button(self.m, text="Submit", command=self.pushPlayers)
+      submitButton.grid(row=2, column=0)
     
-    playerIdEntry.delete(0, END)
-    playerCodenameEntry.delete(0, END)    
-
-validate_id_cmd = m.register(validate_id)
-
-idLabel = Label(m, text="ID")
-codename = Label(m, text="Codename")
-idLabel.grid(row=0, column=0)
-codename.grid(row=0, column=1)
-
-playerIdEntry = Entry(m, validate="key", validatecommand=(validate_id_cmd, '%P'))
-playerCodenameEntry = Entry(m)
-
-playerIdEntry.grid(row=1, column=0)
-playerCodenameEntry.grid(row=1, column=1)
-
-submitButton = Button(text="Submit", command=pushPlayers)
-submitButton.grid(row=2, column=0)
-
-m.mainloop()
+    def run(self):
+        self.m.mainloop()
