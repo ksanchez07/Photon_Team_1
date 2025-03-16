f1da477eed38b96acbf4799d174928ef9891e5cf
ellierichardson25 <egrichar@uark.edu>
2025-03-16 00:29:58 -0500

    player class

---
+++ player.py
@@ -0,0 +1,7 @@
+
+class Player:
+    def __init__(self, player_id, hardware_id, codename, team):
+        self.player_id = player_id
+        self.hardware_id = hardware_id
+        self.codename = codename
+        self.team = team
